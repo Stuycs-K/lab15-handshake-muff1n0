14d453e6542e87b41944b85148b93299226e9aa1
muff1n0 <tchen51@stuy.edu>
2024-12-19 14:50:47 -0500

    add progress on handshakes

--- pipe_networking.c
+++ pipe_networking.c
@@ -10,12 +10,16 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_setup() {
-  int from_client = 0;
+
+  mkfifo(WKP, 0644);
+  int from_client = open(WKP, O_RDONLY);
+  remove(rfile);
+
   return from_client;
 }
 
 /*=========================
-  server_handshake 
+  server_handshake
   args: int * to_client
 
   Performs the server side pipe 3 way handshake.
@@ -24,7 +28,11 @@ int server_setup() {
   returns the file descriptor for the upstream pipe (see server setup).
   =========================*/
 int server_handshake(int *to_client) {
-  int from_client;
+  int from_client = server_setup();
+  int SYN;
+  read(from_client, *SYN, 4);
+  
+
   return from_client;
 }
 
@@ -39,7 +47,19 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  int from_server;
+  char pp[30];
+  sprintf(pp, "%d", getpid());
+  mkfifo(pp, 0644);
+
+  *to_server = open(WKP, O_WRONLY);
+  write(*to_server, getpid(), 30);
+
+
+
+  int from_server = open(pp, O_RDONLY);
+
+
+
   return from_server;
 }
 
@@ -56,5 +76,3 @@ int server_connect(int from_client) {
   int to_client  = 0;
   return to_client;
 }
-
-
