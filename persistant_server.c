fc9b555e552b70730c6c5c34a89e1ccb2cd54797
muff1n0 <tchen51@stuy.edu>
2025-01-03 14:50:17 -0500

    add some persistant server progress yet to test

--- persistant_server.c
+++ persistant_server.c
@@ -5,4 +5,17 @@ int main() {
   int from_client;
 
   from_client = server_handshake( &to_client );
+
+  while (from_client = server_handshake(&to_client)) {
+    srand(time(NULL));
+    while (1) {
+      int x = rand() % 101;
+      write(to_client, &x, 4);
+      printf("Sending %d to client(s)\n", x);
+      sleep(1);
+    }
+
+    close(to_client);
+    close(from_client);
+  }
 }
